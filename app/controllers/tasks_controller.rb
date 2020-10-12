3fdd3053228143193b88990017d9df93136307ff
daiki.tsuji <kiidajitsu0329@yahoo.co.jp>
2020-10-12 09:12:39 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,11 +52,7 @@ end
   def set_task
     @task = Task.find(params[:id])
   end
-  
-  def task_params
-  params.require(:task).permit(:content)
-  end
-  
+
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
