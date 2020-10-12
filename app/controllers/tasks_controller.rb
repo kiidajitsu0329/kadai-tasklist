002e141cd7e0a426c1079ddd379462c62b1b2de2
daiki.tsuji <kiidajitsu0329@yahoo.co.jp>
2020-10-12 01:31:06 +0000

    CRUD kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,20 @@
 class TasksController < ApplicationController
+  
   def index
     @tasks = Task.all
   end
-end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
 
+  def new
+    @task = Task.new
+  end
+  
   def create
     @task = Task.new(task_params)
-
+    
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -48,3 +56,4 @@ end
   def task_params
     params.require(:task).permit(:content)
   end
+end
